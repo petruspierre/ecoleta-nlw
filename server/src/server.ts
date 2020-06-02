2cc74aed3e575f76def9916422c0228dc615f12d
Petrus Pierre <petrus.pierre@academico.ifpb.edu.br>
2020-06-02 15:15:12 -0300

    Database setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  console.log('opa');
-  res.json([
-    'Petrus',
-    'Paulo'
-  ]);
-});
+app.use(express.json())
+
+app.use(routes);
 
 app.listen(3333);
