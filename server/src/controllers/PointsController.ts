d4bdd403a24fd2ce2ef47420ce2e242a8d39bcdc
Petrus Pierre <petrusfla@gmail.com>
2020-06-06 12:43:41 -0300

    Added image upload in server

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -20,7 +20,14 @@ class PointsController {
       .distinct()
       .select('points.*');
 
-    return res.json(points);
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.0.27:3333/uploads/tmp/${point.image}`
+      }
+    })
+
+    return res.json(serializedPoints);
   }
 
   async show(req: Request, res: Response) {
@@ -32,12 +39,17 @@ class PointsController {
       return res.status(400).json({ message: 'Point not found' });
     }
 
+    const serializedPoint = {
+        ...point,
+        image_url: `http://192.168.0.27:3333/uploads/tmp/${point.image}`
+      }
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return res.status(200).json({ point, items })
+    return res.status(200).json({ serializedPoint, items })
   }
 
   async create(req: Request, res: Response) {
@@ -55,7 +67,7 @@ class PointsController {
     const trx = await knex.transaction();
   
     const point = {
-      image: 'https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: req.file.filename,
       name,
       email,
       whatsapp,
@@ -69,7 +81,10 @@ class PointsController {
   
     const point_id = insertedIds[0];
   
-    const pointItems = items.map((item_id: number) => {
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
       return {
         item_id,
         point_id,
